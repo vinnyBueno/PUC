06bb134368fee336e52d6307c4e69ff1b99250c5
vinnyBueno <buenoviny@gmail.com>
2026-03-23 20:21:04 -0300

    Excercicio 36 de atrasar as horas

---
+++ estudos - progrmacao/Python/Lista_exercicios/ex_36.py
@@ -0,0 +1,46 @@
+print("Programa para valider se horas são válidas")
+
+
+try:
+    hora = int(input("Digite hora: "))
+except ValueError:
+    print("\nHora deve ser numerica!")
+else:
+    if hora < 0 or hora > 23:
+        print("\nHorario invalido")
+    else:
+        try:
+            minutos = int(input("\nDigite os minutos: "))
+        except ValueError:
+            print("\nMinuto deve ser numerico!")
+        else:
+            if minutos < 0 or minutos > 59:
+                print("\nMinuto invalido!")
+            else:
+                try:
+                    segundos = int(input("\nDigite segundos: "))
+                except ValueError:
+                    print("\nSegundos deve ser numerico!")
+                else:
+                    if segundos < 0 or segundos > 60:
+                        print("\nSegundos invalidos!")
+                    else:
+                        print(f"\n{hora}:{minutos}:{segundos}")
+                        try:
+                            qtdSegundos = int(input("\nAgora digite quantos segundo você quer adiantar: "))
+                        except ValueError:
+                            print("\nSegundos deve ser numerico")
+                        else:
+
+                            total = hora * 3600 + minutos * 60 + segundos - qtdSegundos
+
+
+                            total = total % 86400
+
+                            hora = total // 3600
+                            total = total % 3600
+
+                            minutos = total // 60
+                            segundos = total % 60
+
+                            print(f"\nNovo horario: {hora:02d}:{minutos:02d}:{segundos:02d}")

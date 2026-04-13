3eaf0f6a427e005fa03a26abf2a9ecc843d57eca
Vinicius <105261698+vinnyBueno@users.noreply.github.com>
2026-04-13 20:47:13 -0300

    tentativas

--- estudos - progrmacao/Python/desafios/ex_trinagulo_bolinhas.py
+++ estudos - progrmacao/Python/desafios/ex_trinagulo_bolinhas.py
@@ -1,22 +1,33 @@
-print("Programa que gera uma pirâmide com a letra O")
-rodaPrograma = True
-while rodaPrograma:
-    try:
-        numero = int(input("\nDigite um número maior ou igual a 2: "))
-    except ValueError:
-        print("Erro: você deve digitar um número inteiro!")
-    else:
-        if numero < 2:
-            print("Erro: o número deve ser maior ou igual a 2")
+def quantidade_bolinhas():
+    rodaPrograma = True
+    while rodaPrograma:
+        try:
+            numero = int(input("\nDigite um número maior ou igual a 2: "))
+        except ValueError:
+            print("Erro: você deve digitar um número inteiro!")
         else:
+            if numero < 2:
+                print("Erro: o número deve ser maior ou igual a 2")
+            else:
+                rodaPrograma = False
+    return numero
 
-            i = 1
-            while i <= numero:
-                espacos = numero - i
-                letras = 2 * i - 1
-                print(" " * espacos, end="")
-                print("O" * letras)
-                i += 1
 
-                if i > numero:
-                    rodaPrograma = False
+def mostra_bolinhas(numero):
+    i = 1
+    while i <= numero:
+        espacos = numero - i
+        letras = 2 * i - 1
+        print(" " * espacos, end="")
+        print("O" * letras)
+        i += 1
+    
+
+print("Programa que gera uma pirâmide com a letra O")
+
+rodaPrograma = False
+while not rodaPrograma:
+    qtdBolas = quantidade_bolinhas()
+
+    resultado = mostra_bolinhas()
+    print(resultado)

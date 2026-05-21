09195873074200690f477f623def1e3d4a9572b8
Vinicius <105261698+vinnyBueno@users.noreply.github.com>
2026-05-21 18:46:12 -0300

    trabalho de tecnologias em ti

---
+++ tecnologias_ti/sketch.ino
@@ -0,0 +1,29 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  if(!Serial) Serial.println("serial is not ok");
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(3, HIGH);
+  Serial.println("green is on");
+  delay(2000);
+  digitalWrite(3, LOW);
+  Serial.println("green is off");
+
+  digitalWrite(4, HIGH);
+  Serial.println("yallow is on");
+  delay(600);
+  digitalWrite(4, LOW);
+  Serial.println("yallow is off");
+
+  digitalWrite(5, HIGH);
+  Serial.println("red is on");
+  delay(1000);
+  digitalWrite(5, LOW);
+  Serial.println("red is off");  
+}
